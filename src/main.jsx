0f4c4565387aa0eb7c780a6fcda98c53ddb43f8b
srivishnuvarthan-07 <kit27.ad54@gmail.com>
2026-01-10 20:20:48 +0530

    feat: adding error handling so it can identify error

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,9 @@
 import { jsx as _jsx } from "react/jsx-runtime";
 import { createRoot } from "react-dom/client";
+import { ErrorBoundary } from "./components/ErrorBoundary";
 import App from "./App.jsx";
 import "./index.css";
-createRoot(document.getElementById("root")).render(_jsx(App, {}));
+
+createRoot(document.getElementById("root")).render(
+    _jsx(ErrorBoundary, { children: _jsx(App, {}) })
+);
