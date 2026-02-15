25c0f707c4b11993dca1a29f39337601c3e0841c
srivishnuvarthan-07 <kit27.ad54@gmail.com>
2026-02-15 21:32:43 +0530

    created a db and quadtree logic for optimization

---
+++ src/lib/storage/db.js
@@ -0,0 +1,34 @@
+import { openDB } from 'idb';
+
+const DB_NAME = 'infinity-canvas-db';
+const STORE_NAME = 'key-val-store';
+const VERSION = 1;
+
+/**
+ * Initialize the Database
+ */
+const dbPromise = openDB(DB_NAME, VERSION, {
+    upgrade(db) {
+        if (!db.objectStoreNames.contains(STORE_NAME)) {
+            db.createObjectStore(STORE_NAME);
+        }
+    },
+});
+
+export const db = {
+    async get(key) {
+        return (await dbPromise).get(STORE_NAME, key);
+    },
+    async set(key, val) {
+        return (await dbPromise).put(STORE_NAME, val, key);
+    },
+    async delete(key) {
+        return (await dbPromise).delete(STORE_NAME, key);
+    },
+    async clear() {
+        return (await dbPromise).clear(STORE_NAME);
+    },
+    async keys() {
+        return (await dbPromise).getAllKeys(STORE_NAME);
+    },
+};
