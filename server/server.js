f159abbd58ffc9015925a7306a5d7f983ead0bc7
srivishnuvarthan-07 <kit27.ad54@gmail.com>
2026-02-22 17:15:15 +0530

    updated a server file

--- server/server.js
+++ server/server.js
@@ -1,13 +1,28 @@
 require('dotenv').config();
 const app = require('./src/app');
 const connectDB = require('./src/config/db');
+const http = require('http');
+const socketIo = require('socket.io');
+const setupSocket = require('./src/socket.handler');
 
 const PORT = process.env.PORT || 5000;
 
 // Connect to Database
 connectDB();
 
-const server = app.listen(PORT, () => {
+const server = http.createServer(app);
+
+const io = socketIo(server, {
+    cors: {
+        origin: process.env.CLIENT_URL || 'http://localhost:5173',
+        methods: ["GET", "POST"],
+        credentials: true
+    }
+});
+
+setupSocket(io);
+
+server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
 
